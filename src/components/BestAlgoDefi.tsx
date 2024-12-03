2675c20d39bb2d27193020e54227baa0765e8ad7
TinyDickLion <tinydicklionsden@gmail.com>
2024-12-03 04:21:32 -0500

    feat: added ipt

--- src/components/BestAlgoDefi.tsx
+++ src/components/BestAlgoDefi.tsx
@@ -6,48 +6,49 @@ import tlp from "../images/tlp.png";
 import rea from "../images/rea.png";
 import rear from "../images/rear.png";
 import bwom from "../images/bwom.png";
+import ipt from "../images/ipt.png";
 
 const tokenData = [
   {
     name: "TDLD",
     logo: tdld, // Replace with actual logo path
     vestigeLink: "https://vestige.fi/asset/2176744157",
-    buyLink: "https://example.com/buy-tdld", // Replace with actual buy link
     xLink: "https://x.com/TinyDickLion",
   },
   {
     name: "NMI",
     logo: nmi, // Replace with actual logo path
     vestigeLink: "https://vestige.fi/asset/2279886826",
-    buyLink: "https://example.com/buy-nmi", // Replace with actual buy link
     xLink: "https://x.com/ConnectionMach",
   },
   {
     name: "BWOM",
     logo: bwom, // Replace with actual logo path
     vestigeLink: "https://vestige.fi/asset/2327984798",
-    buyLink: "https://example.com/buy-nmi", // Replace with actual buy link
     xLink: "https://x.com/bwombus",
+  },
+  {
+    name: "IPT",
+    logo: ipt, // Replace with actual logo path
+    vestigeLink: "https://vestige.fi/asset/2342621554",
+    xLink: "https://x.com/1nfinite0ne",
   },
   {
     name: "TLP",
     logo: tlp, // Replace with actual logo path
     vestigeLink: "https://vestige.fi/asset/987374809",
-    buyLink: "https://example.com/buy-tlp", // Replace with actual buy link
     xLink: "https://x.com/TLPCoin",
   },
   {
     name: "REAR",
     logo: rear, // Replace with actual logo path
     vestigeLink: "https://vestige.fi/asset/2413037774",
-    buyLink: "https://example.com/buy-rear", // Replace with actual buy link
     xLink: "https://x.com/TLPCoin",
   },
   {
     name: "REA",
     logo: rea, // Replace with actual logo path
     vestigeLink: "https://vestige.fi/asset/1753483231",
-    buyLink: "https://example.com/buy-rea", // Replace with actual buy link
     xLink: "https://x.com/TLPCoin",
   },
 ];
@@ -74,14 +75,6 @@ const BestAlgoDefi: React.FC = () => {
               >
                 Vestige
               </a>
-              {/* <a
-                href={token.buyLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.buyButton}
-              >
-                Buy {token.name}
-              </a> */}
               <a
                 href={token.xLink}
                 target="_blank"
