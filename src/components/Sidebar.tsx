7bf4eccaf7d2339dcb97275614e493347768da3a
TinyDickLion <tinydicklionsden@gmail.com>
2024-12-24 23:37:47 -0500

    ref: active component highlight for sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaWallet,
   FaUsers,
@@ -12,80 +12,107 @@ import styles from "../css_modules/SidebarStyles.module.css";
 
 interface SidebarProps {
   onLogout: () => void;
-  setActiveComponent: (component: string) => void; // Add prop for setting the active component
+  setActiveComponent: (component: string) => void;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ onLogout, setActiveComponent }) => {
+  const [activeItem, setActiveItem] = useState<string>("dashboard");
+
+  const handleItemClick = (component: string) => {
+    setActiveItem(component);
+    setActiveComponent(component);
+  };
+
   return (
     <aside className={styles.sideNav}>
       <h3
-        onClick={() => setActiveComponent("dashboard")}
-        className={styles.appName}
+        onClick={() => handleItemClick("dashboard")}
+        className={`${styles.appName} ${
+          activeItem === "dashboard" ? styles.activeNavItem : ""
+        }`}
       >
         Dashboard
       </h3>
       <nav>
         <ul>
           <li
-            className={`${styles.navItem} ${styles.dropdown}`}
-            onClick={() => setActiveComponent("bestAlgoDefi")}
+            className={`${styles.navItem} ${
+              activeItem === "bestAlgoDefi" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleItemClick("bestAlgoDefi")}
           >
             <FaChartBar className={styles.icon} />
             <span>Best Algo Defi</span>
           </li>
           <li
-            className={styles.navItem}
-            onClick={() => setActiveComponent("buySellAAA")}
+            className={`${styles.navItem} ${
+              activeItem === "buySellAAA" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleItemClick("buySellAAA")}
           >
             <FaShoppingCart className={styles.icon} />
             <span>Buy & Sell AAA</span>
           </li>
           <li
-            className={`${styles.navItem} ${styles.dropdown}`}
-            onClick={() => setActiveComponent("stakingAndFarms")}
+            className={`${styles.navItem} ${
+              activeItem === "stakingAndFarms" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleItemClick("stakingAndFarms")}
           >
             <FaCogs className={styles.icon} />
             <span>Staking and Farms</span>
           </li>
           <li
-            className={`${styles.navItem} ${styles.dropdown}`}
-            onClick={() => setActiveComponent("aaaTeam")}
+            className={`${styles.navItem} ${
+              activeItem === "aaaTeam" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleItemClick("aaaTeam")}
           >
             <FaUsers className={styles.icon} />
             <span>AAA Team</span>
           </li>
           <li
-            className={`${styles.navItem} ${styles.dropdown}`}
-            onClick={() => setActiveComponent("donateAAA")}
+            className={`${styles.navItem} ${
+              activeItem === "donateAAA" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleItemClick("donateAAA")}
           >
             <FaDonate className={styles.icon} />
             <span>Donate AAA</span>
           </li>
           <li
-            className={`${styles.navItem} ${styles.dropdown}`}
-            onClick={() => setActiveComponent("verifyAccount")}
+            className={`${styles.navItem} ${
+              activeItem === "verifyAccount" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleItemClick("verifyAccount")}
           >
             <FaCheck className={styles.icon} />
             <span>Verify Account</span>
           </li>
           <li
-            className={`${styles.navItem} ${styles.dropdown}`}
+            className={`${styles.navItem} ${
+              activeItem === "setupWallet" ? styles.activeNavItem : ""
+            }`}
             style={{ backgroundColor: "aquamarine" }}
-            onClick={() => setActiveComponent("setupWallet")}
+            onClick={() => handleItemClick("setupWallet")}
           >
             <FaCogs className={styles.icon} />
             <span>Setup Wallet</span>
           </li>
           <li
-            className={`${styles.navItem} ${styles.dropdown}`}
-            onClick={() => setActiveComponent("myWallet")}
+            className={`${styles.navItem} ${
+              activeItem === "myWallet" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleItemClick("myWallet")}
           >
             <FaWallet className={styles.icon} />
             <span>My Wallet</span>
           </li>
           <li
-            className={styles.navItem}
-            onClick={() => setActiveComponent("myTeam")}
+            className={`${styles.navItem} ${
+              activeItem === "myTeam" ? styles.activeNavItem : ""
+            }`}
+            onClick={() => handleItemClick("myTeam")}
           >
             <FaUsers className={styles.icon} />
             <span>My Team</span>
