8b9018e7c8f6bf53681ecd21d08a0ebd8d863e85
TinyDickLion <tinydicklionsden@gmail.com>
2024-12-30 19:31:34 -0500

    ref: moved token data to constants folder

---
+++ src/constants/tokenData.ts
@@ -0,0 +1,120 @@
+import tdld from "../images/tdld.png";
+import nmi from "../images/nmi.png";
+import tlp from "../images/tlp.png";
+import rea from "../images/rea.png";
+import rear from "../images/rear.png";
+import bwom from "../images/bwom.png";
+import ipt from "../images/ipt.png";
+import wkh from "../images/wkh.png";
+import memo from "../images/memo.png";
+import ogs from "../images/ogs.png";
+import cat from "../images/cat.png";
+import aaa from "../images/aaa.png";
+import marcus from "../images/marcus.png";
+
+export default [
+  {
+    name: "AAA",
+    assetID: "2004387843",
+    logo: aaa,
+    vestigeLink: "https://vestige.fi/asset/2004387843",
+    xLink: "https://x.com/algoadoptaaa",
+    stableTVL: true,
+  },
+  {
+    name: "TDLD",
+    assetID: "2176744157",
+    logo: tdld,
+    vestigeLink: "https://vestige.fi/asset/2176744157",
+    xLink: "https://x.com/TinyDickLion",
+    stableTVL: true,
+  },
+  {
+    name: "NMI",
+    assetID: "2279886826",
+    logo: nmi,
+    vestigeLink: "https://vestige.fi/asset/2279886826",
+    xLink: "https://x.com/ConnectionMach",
+    stableTVL: true,
+  },
+  {
+    name: "BWOM",
+    assetID: "2327984798",
+    logo: bwom,
+    vestigeLink: "https://vestige.fi/asset/2327984798",
+    xLink: "https://x.com/bwombus",
+    stableTVL: true,
+  },
+  {
+    name: "IPT",
+    assetID: "2342621554",
+    logo: ipt,
+    vestigeLink: "https://vestige.fi/asset/2342621554",
+    xLink: "https://x.com/1nfinite0ne",
+    stableTVL: true,
+  },
+  {
+    name: "WKH",
+    assetID: "2573477873",
+    logo: wkh,
+    vestigeLink: "https://vestige.fi/asset/2573477873",
+    xLink: "https://x.com/HODL_TLP",
+    stableTVL: true,
+  },
+  {
+    name: "MEMO",
+    assetID: "885835936",
+    logo: memo,
+    vestigeLink: "https://vestige.fi/asset/885835936",
+    xLink: "https://x.com/AlgoMembers",
+    stableTVL: true,
+  },
+  {
+    name: "SQUAD",
+    assetID: "1421321088",
+    logo: ogs,
+    vestigeLink: "https://vestige.fi/asset/1421321088",
+    xLink: "https://x.com/OG_Squad_ALGO",
+    stableTVL: true,
+  },
+  {
+    name: "CAT",
+    assetID: "1691166331",
+    logo: cat,
+    vestigeLink: "https://vestige.fi/asset/1691166331",
+    xLink: "https://x.com/algo_cats",
+    stableTVL: true,
+  },
+  {
+    name: "TLP",
+    assetID: "987374809",
+    logo: tlp,
+    vestigeLink: "https://vestige.fi/asset/987374809",
+    xLink: "https://x.com/TLPCoin",
+    stableTVL: true,
+  },
+  {
+    name: "REAR",
+    assetID: "2413037774",
+    logo: rear,
+    vestigeLink: "https://vestige.fi/asset/2413037774",
+    xLink: "https://x.com/TLPCoin",
+    stableTVL: true,
+  },
+  {
+    name: "REA",
+    assetID: "1753483231",
+    logo: rea,
+    vestigeLink: "https://vestige.fi/asset/1753483231",
+    xLink: "https://x.com/TLPCoin",
+    stableTVL: true,
+  },
+  {
+    name: "MARCUS",
+    assetID: "2466866698",
+    logo: marcus,
+    vestigeLink: "https://vestige.fi/asset/2466866698",
+    xLink: "https://x.com/MarcusFurrelius",
+    stableTVL: true,
+  },
+];
