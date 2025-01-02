a9bbb5eb1ed9fca5af085d7019d68e4415c9bf4e
TinyDickLion <tinydicklionsden@gmail.com>
2025-01-01 19:19:20 -0500

    ref

--- src/components/AAASwap.tsx
+++ src/components/AAASwap.tsx
@@ -39,7 +39,7 @@ export const AAASwap: React.FC<AAASwapProps> = ({
   platformName = "AAA App",
   platformFeeAccount = "HE7225SD6ZKYO45QWYCE4BZ3ITFEK7WI7XGMAVAMB56FZREJVPMHNRSL2E",
   platformFeePercentage = 0.1,
-  themeVariables = "eyJ0aGVtZSI6ImRhcmsiLCJjb250YWluZXJCdXR0b25CZyI6IndoaXRlIiwid2lkZ2V0QmciOiJnb2xkIiwiaGVhZGVyQnV0b25CZyI6IndoaXRlIiwiaGVhZGVyQnV0b25UZXh0IjoiYmxhY2siLCJoZWFkZXJUaXRsZSI6ImJsYWNrIiwiYm9yZGVyUmFkaXVzU2l6ZSI6Im5vbmUiLCJ0aXRsZSI6IlN3YXAiLCJzaG91bGREaXNwbGF5VGlueW1hbkxvZ28iOmZhbHNlfQ==",
+  themeVariables = "eyJ0aGVtZSI6ImRhcmsiLCJjb250YWluZXJCdXR0b25CZyI6IndoaXRlIiwid2lkZ2V0QmciOiJnb2xkIiwiaGVhZGVyQnV0dG9uQmciOiJnb2xkIiwiaGVhZGVyQnV0dG9uVGV4dCI6ImJsYWNrIiwiaGVhZGVyVGl0bGUiOiJibGFjayIsImNvbnRhaW5lckJ1dHRvblRleHQiOiJibGFjayIsImlmcmFtZUJnIjoiZ29sZCIsImJvcmRlclJhZGl1c1NpemUiOiJub25lIiwidGl0bGUiOiJTd2FwIiwic2hvdWxkRGlzcGxheVRpbnltYW5Mb2dvIjpmYWxzZX0%3D&assetIn=0&assetOut=2004387843&platformFeeAccount=HE7225SD6ZKYO45QWYCE4BZ3ITFEK7WI7XGMAVAMB56FZREJVPMHNRSL2E",
   width = "415px",
   height = "440px",
   tokens = [
