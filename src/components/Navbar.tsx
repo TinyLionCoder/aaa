56d1ccdc50dcaaeb6588fb3f14362cd40ca4b470
TinyDickLion <tinydicklionsden@gmail.com>
2025-01-02 12:12:20 -0500

    ref: bg color for swap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,9 +9,6 @@ const Navbar = () => {
         <Link to="/">Home</Link>
       </div>
       <div className={styles.navLinks}>
-        <Link to="/swap-tokens">Swap Tokens</Link>
-      </div>
-      <div className={styles.navLinks}>
         <Link to="/token-listing">List your Tokens</Link>
       </div>
     </nav>
