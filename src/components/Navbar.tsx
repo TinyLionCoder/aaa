3ba32b52ed35d96dd16d10ea62ee6892d288758f
TinyDickLion <tinydicklionsden@gmail.com>
2025-01-02 12:23:59 -0500

    ref: added swap-tokens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,9 @@ const Navbar = () => {
         <Link to="/">Home</Link>
       </div>
       <div className={styles.navLinks}>
+        <Link to="/swap-tokens">Swap Tokens</Link>
+      </div>
+      <div className={styles.navLinks}>
         <Link to="/token-listing">List your Tokens</Link>
       </div>
     </nav>
