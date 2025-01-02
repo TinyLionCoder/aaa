e187519b8a1c9ae657ce41a865971f551f33e042
TinyDickLion <tinydicklionsden@gmail.com>
2025-01-02 16:20:19 -0500

    ref: nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../css_modules/NavbarStyles.module.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.navLinks}>
-        <Link to="/">Home</Link>
+      <div className={styles.logo}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>
+          AAA APP
+        </Link>
       </div>
-      <div className={styles.navLinks}>
-        <Link to="/swap-tokens">Swap Tokens</Link>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        ☰
       </div>
-      <div className={styles.navLinks}>
-        <Link to="/best-algo-defi">Best Algo Defi</Link>
-      </div>
-      <div className={styles.navLinks}>
-        <Link to="/token-listing">List your Tokens</Link>
+      <div className={`${styles.navLinks} ${menuOpen ? styles.open : ""}`}>
+        <Link to="/swap-tokens" onClick={() => setMenuOpen(false)}>
+          Swap Tokens
+        </Link>
+        <Link to="/best-algo-defi" onClick={() => setMenuOpen(false)}>
+          Best Algo Defi
+        </Link>
+        <Link to="/token-listing" onClick={() => setMenuOpen(false)}>
+          List your Tokens
+        </Link>
       </div>
     </nav>
   );
