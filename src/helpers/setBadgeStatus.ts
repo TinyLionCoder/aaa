d38aac097405f6239fe892284310269a5d52cc10
TinyDickLion <tinydicklionsden@gmail.com>
2025-03-12 01:48:37 -0400

    ref

--- src/helpers/setBadgeStatus.ts
+++ src/helpers/setBadgeStatus.ts
@@ -3,12 +3,16 @@ import { algoIndexerClient } from "../algorand/config";
 
 const assetId = "2004387843"; // AAA Token Asset ID
 const vestigeApiUrl = `https://free-api.vestige.fi/asset/${assetId}/price?currency=usd`;
+export const noWalletMsg = "Connect wallet to see badge / ranking status";
+export const learner = "Learner";
+export const investor = "Investor";
+export const wealthBuilder = "Wealth Builder";
 
 export const setBadgeStatus = async (walletAddress: string | null) => {
   if (!walletAddress) {
     localStorage.setItem(
       "badgeRanking",
-      "Connect wallet to see badge / ranking status"
+      noWalletMsg
     );
     return;
   }
@@ -31,12 +35,12 @@ export const setBadgeStatus = async (walletAddress: string | null) => {
 
     const aaaUsdValue = aaaBalance * currentPrice; // Convert AAA balance to USD value
 
-    let badge = "Learner"; // Default status
+    let badge = learner; // Default status
 
     if (algoBalance >= 200 || aaaUsdValue >= 200) {
-      badge = "Wealth Builder";
+      badge = wealthBuilder;
     } else if (algoBalance >= 100 || aaaUsdValue >= 100) {
-      badge = "Investor";
+      badge = investor;
     }
 
     localStorage.setItem("badgeRanking", badge);
