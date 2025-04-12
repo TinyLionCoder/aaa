24086eff2c34a51ded40ff6e7245bde5fed6667a
TinyDickLion <tinydicklionsden@gmail.com>
2025-04-12 19:37:35 -0400

    ref

--- src/components/SetupAndVerify.tsx
+++ src/components/SetupAndVerify.tsx
@@ -113,7 +113,7 @@ export const SetupAndVerify = ({ userId }: SetupAndVerifyProps) => {
       const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
         from: walletAddress,
         to: verificationAddress,
-        amount: 5000000, // 1 ALGO in microAlgos
+        amount: 6000000, // 1 ALGO in microAlgos
         note: new Uint8Array(Buffer.from("AAA app: Verification Fee")),
         suggestedParams,
       });
@@ -188,7 +188,7 @@ export const SetupAndVerify = ({ userId }: SetupAndVerifyProps) => {
         <>
           <h2 className={styles.subheading}>Step 2: Verify Account</h2>
           <p className={styles.description}>
-            Pay a verification fee of <strong>5 ALGO</strong> to verify your
+            Pay a verification fee of <strong>6 ALGO</strong> to verify your
             account.
           </p>
           <PeraWalletButton
